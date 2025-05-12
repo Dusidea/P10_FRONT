8f4b65a4b972a959cfbded062f29c5a300d009fc
Dusidea <laurie.planes@gmail.com>
2025-05-12 16:15:08 +0200

    user page created and header updated with dynamic div sign in or out

--- my-react-app/src/layouts/Header.jsx
+++ my-react-app/src/layouts/Header.jsx
@@ -1,8 +1,13 @@
+import { useLocation } from "react-router";
 import "../styles/main.css";
 import argentBankLogo from "../assets/argentBankLogo.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
 function Header() {
+  const location = useLocation();
+  const pathname = location.pathname;
+
   return (
     <header>
       <nav className="main-nav">
@@ -14,12 +19,28 @@ function Header() {
           />
           <h1 className="sr-only">Argent Bank</h1>
         </a>
-        <div>
-          <a className="main-nav-item" href="./sign-in">
-            <FontAwesomeIcon icon={faCircleUser} />
-            Sign In
-          </a>
-        </div>
+
+        {/* div sign-in si url = home ou sign-in, opérateur ternaire, sinon div sign-out*/}
+
+        {["/", "/sign-in"].includes(pathname) ? (
+          <div>
+            <a className="main-nav-item" href="./sign-in">
+              <FontAwesomeIcon icon={faCircleUser} className="fa-icon" />
+              Sign In
+            </a>
+          </div>
+        ) : (
+          <div>
+            <a className="main-nav-item" href="./user.html">
+              <FontAwesomeIcon icon={faCircleUser} className="fa-icon" />
+              Tony
+            </a>
+            <a className="main-nav-item" href="./index.html">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </a>
+          </div>
+        )}
       </nav>
     </header>
   );
